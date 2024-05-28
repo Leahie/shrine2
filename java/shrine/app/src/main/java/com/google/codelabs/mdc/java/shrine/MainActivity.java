8c2463488b587499ba653e1b6b21dd20815704b4
Hael <LeahZhang1595995@gmail.com>
2024-05-28 09:35:38 -0700

    New Stuff

--- java/shrine/app/src/main/java/com/google/codelabs/mdc/java/shrine/MainActivity.java
+++ java/shrine/app/src/main/java/com/google/codelabs/mdc/java/shrine/MainActivity.java
@@ -7,7 +7,7 @@ import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity implements NavigationHost {
 
-    @Override
+    @Override       
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.shr_main_activity);
